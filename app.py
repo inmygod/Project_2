ee64e3e360137c88eec870f9712591ab0a63a91c
inmygod <inmygod@hotmail.com>
2024-11-14 18:14:11 +0600

    Bug fixed

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 favourite_foods = [] # Initialize empty List for favourite foods
 while True:
     print("0. Exit")
-    print("Add foods")
-    print("Remove foods")
-    print("View favourite all foods")
+    print("1. Add foods")
+    print("2. Remove foods")
+    print("2. View favourite all foods")
 
     choice = input("Choose an option: ") # From taken user input
 
@@ -17,12 +17,12 @@ while True:
     elif choice == "2":
         food = input("Enter a food name which you want to remove")
         favourite_foods.remove(food)
-        print("Food remove Successfully")
+        print("Food removed Successfully")
     elif choice == "3":
         if favourite_foods:
             print("Your favourite foods:")
             for index, food in enumerate(favourite_foods, start=1):
-                print(f"{index}.{food}")
+                print(f"{index}.{food}") # For printing something dynamic, use {}
         else:
             print("Food List is empty or didn't added yet!")
         
